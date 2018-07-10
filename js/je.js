ee2811644a6766f71bd396cb1f9df7f0b8fa25d1
Elyes012 <maamouri.ilyes@gmail.com>
2018-07-10 16:10:58 +0200

    fase finale du projet

--- js/je.js
+++ js/je.js
@@ -1,3 +1,73 @@
-function affichage() {
-alert('Choisier : Papier, Siceau, Pierre');
+var choiutls = prompt("Choisissez vous pierre, feuille, ou ciseau?");
+var choipc = Math.floor(Math.random() * 11);
+console.log(choiutls);
+console.log(choipc);
+
+if (choiutls === "") {
+    document.write("<h1>saisisez une entre</h1>");
+    
 }
+else{
+    document.write("<h1>Player 1</h1>" +choiutls );
+}
+
+    if (choipc <=5) 
+    {
+choipc  = "pierre";
+    
+    } 
+    else if (choipc >=8 )
+
+    {
+        choipc = "feuille";
+    }
+    else
+    {
+        choipc = "ciseau";
+    }
+    document.write("<h1>Computer </h1>" +choipc );
+
+ 
+
+    
+   var result = function (choix1, choix2)
+    {
+        if ((choix1 === choix2 ))
+         {
+            document.write("<h1>Null</h1>");
+        }
+        else if ((choix1 === "feuille") && (choix2 === "ciseau"))
+        { 
+            document.write("<h1>vous etes gagner/h1>");
+
+        }
+        else if ((choix1 === "feuille") && (choix2 === "pierre"))
+        {
+            document.write("<h1>vous etes gangner</h1>");
+        }
+
+        else if ((choix1 === "pierre") && (choix2 === "ciseau"))
+        {
+            document.write("<h1>vous etes gagner</h1>");
+        }
+
+        else if ((choix1 === "pierre") && (choix2 === "feuille"))
+        {
+            document.write("<h1>vous etes perdu</h1>");
+        }
+
+
+
+
+        else if ((choix1 === "ciseau") && (choix2 === "feuille"))
+        {
+            document.write("<h1>vous etes gagner</h1>");
+        }
+        else if ((choix1 === "ciseau") && (choix2 === "pierre"))
+        {
+            document.write("<h1>vous etes perdu</h1>");
+        }
+    }
+    
+    var res = result(choiutls, choipc);
+    
